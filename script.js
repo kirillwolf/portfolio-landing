ee808aa144be9b69f19f4a504309d895527eaaea
KirilWolf <kirill@Mac-Pro-Kirill.local>
2025-05-24 18:33:16 +0300

    Первый коммит: добавлены HTML и CSS файлы

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("JavaScript работает!");
+
+alert("Добро пожаловать на мой сайт!");
+
+// Кнопка 1 — "Нажми на меня"
+const button = document.getElementById("helloBtn");
+button.addEventListener("click", function () {
+    alert("Привет, Кирилл! Это JavaScript в действии ✨");
+});
+
+// Кнопка 2 — "Сменить тему"
+const themeBtn = document.getElementById("toggleThemeBtn");
+
+// Проверка темы при загрузке
+if (localStorage.getItem("theme") === "dark") {
+    document.body.classList.add("dark-mode");
+}
+
+// Обработчик нажатия
+themeBtn.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+
+    // Сохраняем выбранную тему
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+});
